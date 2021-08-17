ab71a42adbabfd47c79204ff2601a0f26f8e2ef8
Vladyslav Bobeliak <vladislavbobeliak@gmail.com>
2021-08-17 16:31:07 +0300

    task_3

--- cypress/integration/QA_Automation_test_task.js
+++ cypress/integration/QA_Automation_test_task.js
@@ -1,6 +1,11 @@
 
 describe("All the elements are present on the page", () => {
-    it("testing framework which will open", () => {
-        cy.visit('https://www.pecodesoftware.com/qa-portal/greet.php')
+    beforeEach(() => {
+        cy.visit('https://www.pecodesoftware.com/qa-portal/greet.php');
+    })
+    it("Fill in the “Username” and “Password” input fields and click the “LogIn” button", () => {
+        cy.get('input[name="username"]').type(Cypress.env('username'));
+        cy.get('input[name="password"]').type(Cypress.env('password'));
+        cy.get('.btn.btn-success').click();
     })
 })
