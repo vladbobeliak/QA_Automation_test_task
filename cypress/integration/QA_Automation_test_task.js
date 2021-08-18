939664c7bbafa02cdbcb03af27547b7237ee0720
vladbobeliak <45859398+vladbobeliak@users.noreply.github.com>
2021-08-18 23:40:56 +0300

    Add the new logic according to the page object
    Add the new logic according to the page object

--- cypress/integration/QA_Automation_test_task.js
+++ cypress/integration/QA_Automation_test_task.js
@@ -1,59 +1,52 @@
+//import '../pageObjects/loginPage'
+import LogInPage from '../pageObject/loginPage';
+const logInPage = new LogInPage();
+const ENV = Cypress.env()
 
 describe("QA Automation test task", () => {
 
     beforeEach(() => {
-        cy.visit('https://www.pecodesoftware.com/qa-portal/greet.php');
+        logInPage.visit();
     })
 
     it("Fill in the “Username” and “Password” input fields and click the “LogIn” button", () => {
-        cy.get('input[name="username"]').type(Cypress.env('incorrectUsername'));
-        cy.get('input[name="password"]').type(Cypress.env('incorrectPassword'));
-        cy.get('.btn.btn-success').click();
+        logInPage.fillUsername(ENV.incorrectUsername);
+        logInPage.fillPassword(ENV.incorrectPassword);
+        logInPage.logIn();
     })
 
     it("Verify that all the elements are present on the page", () => {
-        cy.get('input[name="username"]').should('exist').and('be.visible');
-        cy.get('input[name="password"]').should('exist').and('be.visible');
-        cy.get('.btn.btn-success').should('exist').and('be.visible');
+        logInPage.verifyElements(logInPage.getUsernameField());
+        logInPage.verifyElements(logInPage.getPasswordField());
+        logInPage.verifyElements(logInPage.getLoginButton());
     })
 
     it("Verify that all the error messages appear (with empty fields “Username” and “Password”)", () => {
-        cy.get('.btn.btn-success').click();
-        cy.get(':nth-child(1) > .help-block').contains("Please enter username.").should('be.visible');
-        cy.get(':nth-child(2) > .help-block').contains("Please enter your password.").should('be.visible');
+        logInPage.logIn();
+        logInPage.verifyError(logInPage.getErrorDueToUsername(), "Please enter username.");
+        logInPage.verifyError(logInPage.getErrorDueToPassword(), "Please enter your password.");
     })
 
     it("Verify that all the error messages appear (with empty field “Password”)", () => {
-        cy.get('input[name="username"]').type(Cypress.env('incorrectUsername'));
-        cy.get('.btn.btn-success').click();
-        cy.get(':nth-child(2) > .help-block').contains("Please enter your password.").should('be.visible');
+        logInPage.fillUsername(ENV.incorrectUsername)
+        logInPage.logIn();
+        logInPage.verifyError(logInPage.getErrorDueToPassword(), "Please enter your password.");
     })
 
     it("Verify that all the error messages appear (with empty field “Username”)", () => {
-        cy.get('input[name="password"]').type(Cypress.env('incorrectPassword'));
-        cy.get('.btn.btn-success').click();
-        cy.get(':nth-child(1) > .help-block').contains("Please enter username.").should('be.visible');
+        logInPage.fillPassword(ENV.incorrectPassword)
+        logInPage.logIn();
+        logInPage.verifyError(logInPage.getErrorDueToUsername(), "Please enter username.");
     })
 
     it("Verify that all the error messages appear (When all fields are filled with incorrect values)", () => {
-        cy.get('input[name="username"]').type(Cypress.env('incorrectUsername'));
-        cy.get('input[name="password"]').type(Cypress.env('incorrectPassword'));
-        cy.get('.btn.btn-success').click();
-        cy.get(':nth-child(1) > .help-block').contains("No account found with that username.").should('be.visible');
+        logInPage.fillUsername(ENV.incorrectUsername)
+        logInPage.fillPassword(ENV.incorrectPassword)
+        logInPage.logIn();
+        logInPage.verifyError(logInPage.getErrorDueToUsername(), "No account found with that username.");
     })
 
     it("Successful login", () => {
-        cy.request({
-            method: 'POST',
-            url: '/qa-portal/registerlogin/registerlogin.php',
-            form: true,
-            body: {
-                username: Cypress.env('correctUsername'),
-                password: Cypress.env('correctPassword'),
-            },
-            }).then(
-                (response) => {
-                    expect(response.status).to.eq(302);
-                })
+        logInPage.loginSuccessRequest();
     })
 })
